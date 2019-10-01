403e1b9d6239dabdbc2e55ad48fbbeedf8894073
zhaochun <zhaochun07@gmail.com>
2019-10-01 11:00:53 -0700

    change clock names

--- tcl/common/clock_variables.tcl
+++ tcl/common/clock_variables.tcl
@@ -11,11 +11,11 @@ set IOM_CLK_NAME   "iom_clk"
 set IOM_CLK_PERIOD 2.5
 
 # main clock running the manycore complex
-set CORE_CLK_NAME   "manycore_clk"
+set CORE_CLK_NAME   "bp_clk"
 set CORE_CLK_PERIOD 2.0
 
 # tag clock
-set TAG_CLK_NAME   "tclk"
+set TAG_CLK_NAME   "tag_clk"
 set TAG_CLK_PERIOD 10.0
 
 puts "BSG-info: Completed script [info script]\n"

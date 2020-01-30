1f41b62dac67ae2df7c59fccb2e1a0af7607c5c0
zhaochun <zhaochun07@gmail.com>
2020-01-30 14:43:35 -0800

    change the way to find library cells to set dont_use

--- toplevels/bsg_ac_black_parrot_quad_core_rc3/tcl/common/library_dont_use_file.tcl
+++ toplevels/bsg_ac_black_parrot_quad_core_rc3/tcl/common/library_dont_use_file.tcl
@@ -1,19 +1,11 @@
 puts "BSG-info: Running script [info script]\n"
 
-foreach_in_collection lib [get_libs tcbn*] {
-  set lib_name [get_attribute $lib name]
-  set dont_use_lib_cells [get_lib_cells $lib_name/TIE*]
-  if { [sizeof $dont_use_lib_cells] > 0 } { set_dont_use $dont_use_lib_cells }
-  set dont_use_lib_cells [get_lib_cells $lib_name/L*]
-  if { [sizeof $dont_use_lib_cells] > 0 } { set_dont_use $dont_use_lib_cells }
-  set dont_use_lib_cells [get_lib_cells $lib_name/DEL*]
-  if { [sizeof $dont_use_lib_cells] > 0 } { set_dont_use $dont_use_lib_cells }
-  set dont_use_lib_cells [get_lib_cells $lib_name/BUFTD*]
-  if { [sizeof $dont_use_lib_cells] > 0 } { set_dont_use $dont_use_lib_cells }
-  set dont_use_lib_cells [get_lib_cells $lib_name/BHD*]
-  if { [sizeof $dont_use_lib_cells] > 0 } { set_dont_use $dont_use_lib_cells }
-  set dont_use_lib_cells [get_lib_cells $lib_name/DFKCSND*]
-  if { [sizeof $dont_use_lib_cells] > 0 } { set_dont_use $dont_use_lib_cells }
-}    
+set_dont_use [get_lib_cells */* -filter "name=~TIE*"]
+set_dont_use [get_lib_cells */* -filter "name=~LH*"]
+set_dont_use [get_lib_cells */* -filter "name=~LN*"]
+set_dont_use [get_lib_cells */* -filter "name=~DEL*"]
+set_dont_use [get_lib_cells */* -filter "name=~BUFTD*"]
+set_dont_use [get_lib_cells */* -filter "name=~BHD*"]
+set_dont_use [get_lib_cells */* -filter "name=~DFKCSND*"]
 
 puts "BSG-info: Completed script [info script]\n"

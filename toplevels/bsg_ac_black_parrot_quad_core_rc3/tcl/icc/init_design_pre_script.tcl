b174d0a497314227ab7a6900d5a0119f3c70253c
Tommy Jung <tommy.dc.jung@gmail.com>
2020-04-01 14:32:21 -0700

    add check_timing in init_design pre script

--- toplevels/bsg_ac_black_parrot_quad_core_rc3/tcl/icc/init_design_pre_script.tcl
+++ toplevels/bsg_ac_black_parrot_quad_core_rc3/tcl/icc/init_design_pre_script.tcl
@@ -4,4 +4,6 @@ if { $ICC_IMPLEMENTATION_PHASE == "default" } {
   set_false_path -to [get_pins -of_objects [get_fp_cells -filter "is_hard_macro"] -filter "name==CLKR||name==CLKW"]
 }
 
+redirect -file $REPORTS_DIR_INIT_DESIGN/init_design_icc.check_timing.rpt {check_timing}
+
 puts "Flow-Info: Completed script [info script]\n"

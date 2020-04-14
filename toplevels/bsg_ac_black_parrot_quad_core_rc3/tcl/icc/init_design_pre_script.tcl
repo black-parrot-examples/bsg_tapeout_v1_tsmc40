f86bf2847bfa7db84eb910d703904fdfc230b24e
zhaochun <zhaochun07@gmail.com>
2020-04-13 21:45:51 -0700

    enabling the constraints in all scenarios

--- toplevels/bsg_ac_black_parrot_quad_core_rc3/tcl/icc/init_design_pre_script.tcl
+++ toplevels/bsg_ac_black_parrot_quad_core_rc3/tcl/icc/init_design_pre_script.tcl
@@ -1,10 +1,13 @@
 puts "Flow-Info: Running script [info script]\n"
 
-if { $ICC_IMPLEMENTATION_PHASE == "default" } {
-  set_false_path -to [get_pins -of_objects [get_fp_cells -filter "is_hard_macro"] -filter "name==CLKR||name==CLKW"]
-}
-
 set cur_scenario [current_scenario]
+
+if { $ICC_IMPLEMENTATION_PHASE == "default" } {
+  foreach scenario [all_active_scenarios] {
+    current_scenario $scenario
+    set_false_path -to [get_pins -of_objects [get_fp_cells -filter "is_hard_macro"] -filter "name==CLKR||name==CLKW"]
+  }
+}
 
 # setup scenario check_timing
 foreach scenario [get_scenarios -setup true] {

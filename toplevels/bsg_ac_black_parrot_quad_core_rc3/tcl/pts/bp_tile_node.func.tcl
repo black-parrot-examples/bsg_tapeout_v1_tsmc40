1f931db98de66b7ac26f63214fb82ce619984689
Tommy Jung <tommy.dc.jung@gmail.com>
2020-04-15 14:30:09 -0700

    setup sta for block-level; pick the correct sdc

--- toplevels/bsg_ac_black_parrot_quad_core_rc3/tcl/pts/bp_tile_node.func.tcl
+++ toplevels/bsg_ac_black_parrot_quad_core_rc3/tcl/pts/bp_tile_node.func.tcl
@@ -1,6 +1,16 @@
 puts "BSG-info: Running script [info script]\n"
 
-read_sdc -echo $DESIGN_NAME.${signoff_mode}_wc_cworst.output.sdc
+set opcond [lindex [split $corner _] 0] 
+
+if {$opcond == "bc"} {
+  read_sdc -echo $DESIGN_NAME.${signoff_mode}_bc_cbest.output.sdc
+} elseif {$opcond == "wc"} {
+  read_sdc -echo $DESIGN_NAME.${signoff_mode}_wc_cworst.output.sdc
+} else {
+  puts "Error: unexpected operating condition."
+  exit 1
+}
+
 remove_clock_uncertainty [all_clocks]
 reset_timing_derate
 remove_max_transition  [current_design]

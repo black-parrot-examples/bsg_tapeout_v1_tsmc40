f90a9c4419571064b2916232469016362b948037
zhaochun <zhaochun07@gmail.com>
2020-07-27 12:51:38 -0700

    Adding additional common settings for dc & icc

---
+++ toplevels/bsg_ac_black_parrot_quad_core_rc3/tcl/common/additional_common_settings_dc.tcl
@@ -0,0 +1,8 @@
+puts "BSG-info: Running script [info script]\n"
+
+set_app_var hdlin_ff_always_sync_set_reset  true
+set_app_var hdlin_ff_always_async_set_reset false
+
+set_app_var case_analysis_propagate_through_icg true
+
+puts "BSG-info: Completed script [info script]\n"
---
+++ toplevels/bsg_ac_black_parrot_quad_core_rc3/tcl/common/additional_common_settings_icc.tcl
@@ -0,0 +1,5 @@
+puts "BSG-info: Running script [info script]\n"
+
+set_app_var case_analysis_propagate_through_icg true
+
+puts "BSG-info: Completed script [info script]\n"
